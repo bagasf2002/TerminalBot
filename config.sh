2e0c5b638acd0dd0486708b02796585ea665bb27
Mikotoxyz <90189455+Domaraa@users.noreply.github.com>
2022-06-21 00:53:55 +0700

    Freyr

--- config.sh
+++ config.sh
@@ -25,3 +25,5 @@ echo "NOTICE: config.sh is runnning"
 
 wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
 tar -xvf gdrive_2.1.1_linux_386.tar.gz
+
+npm install -g freyr
